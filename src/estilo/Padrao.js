8131b1cf1b2eaf05b81d5850aeecbe28c7b07778
Eraldo Henrique Maia <55844931+EraldoMaia@users.noreply.github.com>
2020-06-05 17:27:06 -0300

    teste

---
+++ src/estilo/Padrao.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+  ex: {
+    paddingHorizontal: 15,
+    marginVertical: 5,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: 'purple',
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
+})
